bfb1864d2e609870c9c4fad946e8b7700c9c3c08
HachemMaadalla <hachem.maadalla@hotmail.com>
2023-09-17 12:31:55 -0700

    main.h

--- main.h
+++ main.h
@@ -2,10 +2,5 @@
 #define MAIN_H
 #include <stdarg.h>
 #include <unistd.h>
-/**
- * _printf - printf function
- * @format: format of the input
- * Return: Print the input
- */
 int _printf(const char *format, ...);
-#endif /* MAIN_H */
+#endif
